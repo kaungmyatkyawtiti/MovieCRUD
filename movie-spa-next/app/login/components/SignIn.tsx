86f586f6b454aaf68e8556c241bbdbb905f6ea84
kaungmyatkyawtiti <eucalyptustiti2003@gmail.com>
2025-08-21 05:07:43 +0630

    chore: add simple movie project redux+spa

---
+++ movie-spa-next/app/login/components/SignIn.tsx
@@ -0,0 +1,159 @@
+'use client';
+
+import {
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Stack,
+  Link,
+  Card,
+} from '@mui/material';
+import * as yup from "yup"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { Controller, useForm } from "react-hook-form";
+import { InferType } from 'yup';
+import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+import { login, selectAuthToken } from '@/lib/features/auth/authSlice';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+const userSchema = yup.object({
+  username: yup.string()
+    .required("username is required"),
+  password: yup
+    .string()
+    // .min(6, "password must be at least 6 characters")
+    .required("password is required"),
+})
+  .required();
+
+type FormData = InferType<typeof userSchema>;
+
+export default function SignIn() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const redirectUrl = searchParams.get("redirectUrl");
+
+  const dispatch = useAppDispatch();
+  const authToken = useAppSelector(selectAuthToken);
+
+  useEffect(() => {
+    if (authToken) {
+      if (redirectUrl) {
+        router.push(redirectUrl);
+      } else {
+        router.push("/");
+      }
+    }
+  }, []);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setError,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(userSchema),
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log('Sign in data:', data);
+
+    dispatch(login(data))
+      .then(
+        success => {
+          console.log("success", success);
+          // router.push("/");
+          if (redirectUrl) {
+            router.push(redirectUrl);
+          } else {
+            router.push("/");
+          }
+        },
+        error => {
+          console.log("error", error);
+          setError("username", {
+            type: "server",
+            message: "Invalid username or password",
+          });
+          setError("password", {
+            type: "server",
+            message: "Invalid username or password",
+          });
+          reset(
+            { username: "", password: "" },
+            { keepErrors: true }
+          );
+        }
+      )
+  };
+
+  return (
+    <>
+      <Stack
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: '60vh' }}>
+        <Card sx={{ maxWidth: 400, width: '100%', p: 4 }}>
+          <Typography variant="h5" component="h1" gutterBottom>
+            Login Your Account
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit(onSubmit)}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <TextField
+
+              {...register("username")}
+              label="Username"
+              type="text"
+              variant="outlined"
+
+              error={!!errors.username}
+              helperText={errors.username?.message}
+              fullWidth
+              margin="normal"
+            />
+
+            <TextField
+              {...register("password")}
+              label="Password"
+              type="password"
+              variant="outlined"
+
+              error={!!errors.password}
+              helperText={errors.password?.message}
+              fullWidth
+              margin="normal"
+            />
+            {/* <FormControlLabel */}
+            {/*   control={<Checkbox value="remember" color="primary" />} */}
+            {/*   label="I agree to the terms and conditions" */}
+            {/* /> */}
+
+            <Button type="submit" variant="contained" fullWidth sx={{ my: 1 }}>
+              Sign in
+            </Button>
+
+            <Link href="#" variant="body2" sx={{ alignSelf: 'center' }}>
+              {/* Already have an account? Sign in */}
+              Don’t have an account? Create one
+            </Link>
+          </Box>
+        </Card>
+      </Stack>
+    </>
+  );
+}
---
+++ movie-spa-next/app/movies/page.tsx
@@ -0,0 +1,151 @@
+'use client';
+
+import { useGetAllMoviesQuery } from "@/lib/features/movie/moviesApiSlice";
+import { Box, Button, CircularProgress, Stack, Typography } from "@mui/material";
+import {
+  Refresh as RefreshIcon
+} from "@mui/icons-material";
+import Loading from "@/app/loading";
+import IsAuth from "@/app/auth/IsAuth";
+import MovieList from "./components/MovieList";
+import MovieEntry from "./components/MovieEntry";
+import { useState } from 'react';
+
+// let movies: Movie[] = [
+//   {
+//     "_id": "684a964b5749dd65e7b29990",
+//     "title": "Inception 3",
+//     "director": {
+//       "name": "Christopher Nolan",
+//       "phoneNo": "123-456-7890",
+//       "_id": "684bcd34b758682478c9a5d7"
+//     },
+//     "year": 2019
+//   },
+//   {
+//     "_id": "684b1fd39bf638816f8e959d",
+//     "title": "Inception",
+//     "director": {
+//       "name": "Christopher Nolan",
+//       "phoneNo": "123-456-7890",
+//       "_id": "684b1fd39bf638816f8e959e" },
+//     "year": 2010,
+//   }
+// ]
+
+type CenteredMessageProps = {
+  children: React.ReactNode;
+  color?: string;
+};
+
+const CenteredMessage = ({
+  children,
+  color
+}: CenteredMessageProps) => (
+  <Box
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    minHeight="200px"
+    sx={{ px: 2, textAlign: 'center' }}
+  >
+    <Typography color={color}>
+      {children}
+    </Typography>
+  </Box>
+);
+
+function MoviePage() {
+  const { data, isError, isLoading, isSuccess, refetch, isFetching } = useGetAllMoviesQuery();
+  // useGetAllMoviesQuery(undefined, {
+  //   pollingInterval: 300000,
+  //   skipPollingIfUnfocused: true,
+  // });
+
+  // const refreshHandler = () => {
+  //   console.log("refresh");
+  //   refetch();
+  // }
+  // console.log("data", data);
+
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const refreshHandler = async () => {
+    console.log("refresh");
+
+    setIsRefreshing(true);
+
+    const delay = new Promise((resolve, reject) =>
+      setTimeout(resolve, 3000)
+    );
+
+    await Promise.all([refetch(), delay]);
+
+    setIsRefreshing(false);
+  };
+
+  const isSpinning = isLoading || isRefreshing;
+
+  return (
+    <Box p={3}>
+      <Stack direction="row" justifyContent="space-between" alignItems="center" mb={2}>
+        <Typography variant="h4" fontWeight="bold">
+          🎬 My Movie Collections
+        </Typography>
+        <Button
+          variant="outlined"
+          size="small"
+          startIcon={
+            isRefreshing
+              ? <CircularProgress size={16} color="inherit" />
+              : <RefreshIcon />
+          }
+          onClick={refreshHandler}
+          sx={{
+            textTransform: 'none',
+            fontWeight: 500,
+          }}
+        >
+          Refresh
+        </Button>
+      </Stack>
+
+      <Box sx={{ p: 3 }}>
+
+        {/* New Movie From Section */}
+        <MovieEntry />
+
+        {
+          isSpinning && <Loading />
+        }
+
+        {
+          isError && isSpinning &&
+          < CenteredMessage color="error">
+            Error loading movies. Please try again.
+          </CenteredMessage>
+        }
+
+        {
+          isSuccess && !isRefreshing && (
+            data?.length === 0
+              ? <CenteredMessage color="text.secondary">No movies found.</CenteredMessage>
+              : (
+                <Stack
+                  direction="row"
+                  spacing={2}
+                  useFlexGap
+                  flexWrap="wrap"
+                  justifyContent="center"
+                >
+                  <MovieList movies={data} />
+                </Stack>
+              )
+          )
+        }
+      </Box>
+    </Box >
+  )
+}
+
+export default IsAuth(MoviePage);

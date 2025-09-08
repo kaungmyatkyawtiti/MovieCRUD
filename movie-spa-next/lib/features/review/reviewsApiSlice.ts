0bd7ca31c2468be42edd5ae9db9c7717e877fcc2
kaungmyatkyawtiti <eucalyptustiti2003@gmail.com>
2025-09-08 15:07:25 +0630

    refactor(reviewsapislice.ts): clean up getAllReviews query config

--- movie-spa-next/lib/features/review/reviewsApiSlice.ts
+++ movie-spa-next/lib/features/review/reviewsApiSlice.ts
@@ -29,6 +29,7 @@ export const reviewsApiSlice = createApi({
   endpoints: (build) => ({
     getAllReviews: build.query<Review[], void>({
       query: () => `/reviews`,
+      keepUnusedDataFor: 600,
       transformResponse: (response: { data: Review[] }, meta, arg) => response.data,
     }),
 
